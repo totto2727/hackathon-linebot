5e119045bea0fa6816acbc2d9fb148da179da7b4
totto2727 <kaihatu.totto2727>
2020-12-21 18:45:05 +0900

    test

---
+++ src/main/java/com/example/linebot/Push.java
@@ -0,0 +1,14 @@
+package com.example.linebot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+public class Push {
+    @GetMapping("test")
+    public String hello(HttpServletRequest request){
+        return "Get from "+request.getRequestURI();
+    }
+}
