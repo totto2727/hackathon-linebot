de0122242136b5919f559d73175b5fa97e41e8a8
totto2727 <kaihatu.totto2727>
2020-12-23 00:22:23 +0900

    連携機能実装

--- src/main/java/com/example/linebot/firebase/FirestoreService.java
+++ src/main/java/com/example/linebot/firebase/FirestoreService.java
@@ -1,17 +1,13 @@
 package com.example.linebot.firebase;
 
-import com.google.api.core.ApiFuture;
 import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.firestore.DocumentReference;
 import com.google.cloud.firestore.Firestore;
-import com.google.cloud.firestore.WriteResult;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
@@ -19,14 +15,10 @@ public class FirestoreService {
     Firestore db;
 
     public FirestoreService() throws IOException {
-        FileInputStream serviceAccount =
-                new FileInputStream("./timetable-2a507-firebase-adminsdk-cftah-5ae491cb19.json");
-
-        FirebaseOptions options = new FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .setDatabaseUrl("https://timetable-2a507.firebaseio.com")
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.getApplicationDefault())
+                .setDatabaseUrl("https://timetable-2a507.firebaseio.com/")
                 .build();
-
         FirebaseApp.initializeApp(options);
         db = FirestoreClient.getFirestore();
     }
