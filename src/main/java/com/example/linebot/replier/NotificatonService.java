80a5e592dcd42d82d99d2624b41177ba7cb2b2ca
totto2727 <kaihatu.totto2727@gmail.com>
2021-11-28 00:16:57 +0900

    モックアカウント追加

--- src/main/java/com/example/linebot/replier/NotificatonService.java
+++ src/main/java/com/example/linebot/replier/NotificatonService.java
@@ -24,7 +24,7 @@ public class NotificatonService {
 
     @Scheduled(cron = "0 * * * * *", zone = "Asia/Tokyo")
     public void pushNotification() {
-        var pushMessage = new PushMessage("", new TextMessage("食べ物の賞味期限が2022/10/28に切れます"));
+        var pushMessage = new PushMessage("Ue68892dce0f152aa5c636830c283fd66", new TextMessage("食べ物の賞味期限が2022/10/28に切れます"));
         try {
             var response = lineMessagingClient.pushMessage(pushMessage).get();
             log.info("Sent messages: {}", response);
