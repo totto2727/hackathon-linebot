888358602d83b7fefbbed9a3feaef4fd0d5d4351
totto2727 <kaihatu.totto2727@gmail.com>
2021-11-28 01:45:29 +0000

    コールバック修正

--- src/main/java/com/example/linebot/Callback.java
+++ src/main/java/com/example/linebot/Callback.java
@@ -34,7 +34,7 @@ public class Callback {
         if (text.equals("期限切れ"))
             return new Notification(event).reply();
         if (text.equals("期限一覧"))
-            new AllTerm(event).reply();
+            return new AllTerm(event).reply();
         return new TextMessage(
                 "連携情報: 連携中のUIDを表示\n");
     }
